04d5c21ccaa137a60c5da97fb23a67277b60e2a1
Jan Jezek <mail@mediatainment-productions.com>
2013-12-13 23:34:16 +0100

    removed css

---
+++ surveys/global-nights.rb
@@ -0,0 +1,15 @@
+survey "The global-nights.com Startup Survey" do
+  section "Basics about you" do
+    label "Please tell us a bit about you"
+
+    q "What is your birth date?"
+    a :date
+
+    q "At what time were you born?"
+    a :time
+
+    q "When would you like to schedule your next appointment?"
+    a :datetime
+
+  end
+end
